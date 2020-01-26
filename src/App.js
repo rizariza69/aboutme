149f2536ad101027c9a9e328c58486457c06f36a
rizariza69 <riza.riza69@gmail.com>
2020-01-26 18:00:42 +0700

    create header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Header from "../src/components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <h1>HOLLLAAAa</h1>
     </div>
   );
