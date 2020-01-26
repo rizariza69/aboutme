1f4a8c7f6141a9c50933141c3383b9914ad4e51a
rizariza69 <riza.riza69@gmail.com>
2020-01-26 22:46:42 +0700

    adding social media and styling in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,30 @@
 import React from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
 
 const Footer = () => {
   const Copyright = styled.div`
     display: flex;
     justify-content: center;
     background-color: black;
+    flex-direction: column;
+    align-items:center;
     /* height: 20px; */
     padding: 10px;
   `;
   return (
     <div id="footer">
       <Copyright>
+        <div style={{marginBottom:" 30px", marginTop:"30px"}}>
+          <a href="https://github.com/rizariza69">
+          <FontAwesomeIcon icon={faGithub}  style={{fontSize:"45px", color:"white", marginRight: "20px", marginLeft:"20px"}}      />
+          </a>
+          <a href="https://www.linkedin.com/in/m-riza/">
+          <FontAwesomeIcon icon={faLinkedin}  style={{fontSize:"45px", color:"white", marginRight: "20px", marginLeft:"20px"}}      />
+          </a>
+        </div>
         <p
           style={{ color: "white", justifyContent: "center", fontSize: "15px" }}
         >
